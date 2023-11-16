058790a92a37f3b62494eeef30559ea285678aee
unknown <asim.dev672@gmail.com>
2023-11-16 12:01:24 +0500

    my-dashboard

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { BsFillChatDotsFill } from "react-icons/bs";
-import CompA5 from "./components/CompA5";
-import CompA6 from "./components/CompA6";
-import CompA7 from "./components/CompA7";
-import CompA8 from "./components/CompA8";
-import { useState } from "react";
-import Chat from "./components/Chat";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home";
+
 function App() {
-  const [show, setShow] = useState(false);
   return (
     <>
-      <ToastContainer />
-      <div className="">
-        <h1>Annex A</h1>
-        <CompA5 />
-        <br />
-        <CompA6 />
-        <br />
-        <CompA7 />
-        <br />
-        <CompA8 />
-      </div>
-      {show ? (
-        <div className="chatBox">
-          <Chat />
-        </div>
-      ) : (
-        ""
-      )}
-      <div className="chat-main">
-        <BsFillChatDotsFill
-          className="chat-ico"
-          onClick={() => setShow(!show)}
-        />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
